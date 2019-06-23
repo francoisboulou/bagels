306a33c4bb4ff10d560c4142ee88ddd3b0ded862
francoisboulou <giscardboulou@gmail.com>
2019-06-23 14:29:32 -0600

    Updates

---
+++ precache-manifest.ca35a6bc31e2f69d4d7efa77981a3510.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6215733280ed919fcd919c52f61ded",
+    "url": "/francoisboulou/bagels/index.html"
+  },
+  {
+    "revision": "0f9325e010c86cc3d578",
+    "url": "/francoisboulou/bagels/static/css/main.5537f6aa.chunk.css"
+  },
+  {
+    "revision": "ea43ab24bf87d710d3b2",
+    "url": "/francoisboulou/bagels/static/js/2.dcb5eaf8.chunk.js"
+  },
+  {
+    "revision": "0f9325e010c86cc3d578",
+    "url": "/francoisboulou/bagels/static/js/main.da34b2aa.chunk.js"
+  },
+  {
+    "revision": "5e0c4c8fb3fe40c80399",
+    "url": "/francoisboulou/bagels/static/js/runtime~main.93aee7d6.js"
+  }
+]);
